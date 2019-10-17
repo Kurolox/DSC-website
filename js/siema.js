fc1e153778d3850bd7a1dbbc6259dd77a8b4edad
Carlos "Kurolox" Gomez <contact@carlosg.dev>
2019-10-17 10:32:26 +0200

    Updated images and text, added event carousel

---
+++ js/siema.js
@@ -0,0 +1,15 @@
+new Siema({
+    selector: ".carousel",
+    duration: 200,
+    easing: "ease-out",
+    perPage: 1,
+    startIndex: 0,
+    draggable: true,
+    multipleDrag: false,
+    threshold: 20,
+    loop: true,
+    rtl: false,
+    onInit: () => {},
+    onChange: () => {},
+  });
+  
